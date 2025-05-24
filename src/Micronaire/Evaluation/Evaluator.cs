3c5afa97484975a2a28d9c8cb3e7c01d2d74632c
DanCodes1414 <danyal.qureshi1414@gmail.com>
2025-05-24 06:22:11 +0100

    Remove unneeded config

--- src/Micronaire/Evaluation/Evaluator.cs
+++ src/Micronaire/Evaluation/Evaluator.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT License.
 
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Xml;
 using Micronaire.Claims;
 using Micronaire.GroundTruth;
 using Micronaire.LLMEvaluation;
