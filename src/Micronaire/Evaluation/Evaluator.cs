9d801a08ca8c40cfaa536af8d3e2fa3e9120b9d9
Danyal Qureshi <danyal.qureshi1414@gmail.com>
2025-05-24 05:51:30 +0100

    Update Evaluator.cs

--- src/Micronaire/Evaluation/Evaluator.cs
+++ src/Micronaire/Evaluation/Evaluator.cs
@@ -1,15 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-using System.Net;
 using Micronaire.Claims;
-using Micronaire.GenerationClaimEvaluation;
 using Micronaire.GroundTruth;
 using Micronaire.LLMEvaluation;
 using Micronaire.OverallClaimEvaluation;
 using Micronaire.RetrievalClaimEvaluation;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Http.Resilience;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Newtonsoft.Json;
@@ -24,43 +19,36 @@ public class Evaluator : IEvaluator
     private readonly ILogger<Evaluator> _logger;
     private readonly ILLMEvaluator _llmEvaluator;
     private readonly IClaimExtractor _claimExtractor;
-    private readonly IConfiguration _configuration;
     private readonly IOverallClaimEvaluator _overallClaimEvaluator;
     private readonly IRetrievalClaimEvaluator _retrievalClaimEvaluator;
-    private readonly IGenerationClaimEvaluator _generationClaimEvaluator;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="Evaluator"/> class.
     /// </summary>
-    /// <param name="configuration">Configuration to use.</param>
     public Evaluator(
         ILogger<Evaluator> logger,
-        IConfiguration configuration,
         IClaimExtractor claimExtractor,
         ILLMEvaluator llmEvaluator,
         IOverallClaimEvaluator overallClaimEvaluator,
-        IRetrievalClaimEvaluator retrievalClaimEvaluator,
-        IGenerationClaimEvaluator generationClaimEvaluator
+        IRetrievalClaimEvaluator retrievalClaimEvaluator
     )
     {
         _logger = logger;
-        _configuration = configuration;
         _claimExtractor = claimExtractor;
         _llmEvaluator = llmEvaluator;
         _overallClaimEvaluator = overallClaimEvaluator;
         _retrievalClaimEvaluator = retrievalClaimEvaluator;
-        _generationClaimEvaluator = generationClaimEvaluator;
     }
 
     /// <inheritdoc/>
     public async Task<EvaluationReport> EvaluateAsync(
+        Kernel evaluator,
         IRagPipeline pipeline,
         string groundTruthPath,
         CancellationToken cancellationToken = default
     )
     {
         var questionReports = new List<QuestionReport>();
-        var evaluator = BuildEvaluator();
         _logger.LogInformation("Loading ground truth from {path}", groundTruthPath);
         foreach (
             var (question, groundTruthAnswer) in GroundTruthLoader.LoadQADataSet(groundTruthPath)
@@ -122,15 +110,6 @@ public class Evaluator : IEvaluator
                 contextClaims,
                 cancellationToken
             );
-            var generationClaimReport =
-                await _generationClaimEvaluator.EvaluateGeneratorMetricsAsync(
-                    evaluator,
-                    contextClaims,
-                    contextClaimsRaw,
-                    generatedClaims,
-                    groundTruthClaims,
-                    cancellationToken
-                );
             questionReports.Add(
                 new QuestionReport()
                 {
@@ -138,7 +117,6 @@ public class Evaluator : IEvaluator
                     LLMReport = llmReport,
                     OverallClaimReport = overallClaimReport,
                     RetrievalClaimReport = retrievalClaimReport,
-                    GenerationClaimReport = generationClaimReport,
                 }
             );
         }
@@ -171,26 +149,7 @@ public class Evaluator : IEvaluator
         var averageRetrievalClaimReport = new RetrievalClaimReport()
         {
             ClaimRecall = questionReports.Average(q => q.RetrievalClaimReport.ClaimRecall),
-            ContextPrecision = questionReports.Average(q =>
-                q.RetrievalClaimReport.ContextPrecision
-            ),
-        };
-        var averageGenerationClaimReport = new GenerationClaimReport()
-        {
-            Faithfulness = questionReports.Average(q => q.GenerationClaimReport.Faithfulness),
-            RelevantNoiseSensitivity = questionReports.Average(q =>
-                q.GenerationClaimReport.RelevantNoiseSensitivity
-            ),
-            IrrelevantNoiseSensitivity = questionReports.Average(q =>
-                q.GenerationClaimReport.IrrelevantNoiseSensitivity
-            ),
-            Hallucination = questionReports.Average(q => q.GenerationClaimReport.Hallucination),
-            SelfKnowledgeScore = questionReports.Average(q =>
-                q.GenerationClaimReport.SelfKnowledgeScore
-            ),
-            ContextUtilization = questionReports.Average(q =>
-                q.GenerationClaimReport.ContextUtilization
-            ),
+            ContextPrecision = questionReports.Average(q => q.RetrievalClaimReport.ContextPrecision),
         };
 
         return new()
@@ -199,60 +158,6 @@ public class Evaluator : IEvaluator
             AverageLLMReport = averageLLMReport,
             AverageOverallClaimReport = averageOverallClaimReport,
             AverageRetrievalClaimReport = averageRetrievalClaimReport,
-            AverageGenerationClaimReport = averageGenerationClaimReport,
         };
-    }
-
-    private Kernel BuildEvaluator()
-    {
-        var evaluatorKernelConfig =
-            _configuration
-                .GetRequiredSection(nameof(EvaluatorAzureOpenAIConfig))
-                .Get<EvaluatorAzureOpenAIConfig>()
-            ?? throw new NullReferenceException($"Missing {nameof(EvaluatorAzureOpenAIConfig)}");
-        var evaluatorKernelBuilder = Kernel
-            .CreateBuilder()
-            .AddAzureOpenAIChatCompletion(
-                evaluatorKernelConfig.ChatModelDeploymentName,
-                evaluatorKernelConfig.Endpoint,
-                evaluatorKernelConfig.ApiKey
-            );
-        evaluatorKernelBuilder.Services.ConfigureHttpClientDefaults(c =>
-        {
-            // Use a standard resiliency policy, augmented to retry on 401 Unauthorized for this example
-            c.AddStandardResilienceHandler()
-                .Configure(o =>
-                {
-                    o.TotalRequestTimeout.Timeout = TimeSpan.FromMinutes(2);
-                    o.Retry.ShouldHandle = args =>
-                        ValueTask.FromResult(
-                            args.Outcome.Result?.StatusCode is HttpStatusCode.Unauthorized
-                                || args.Outcome.Result?.StatusCode is HttpStatusCode.TooManyRequests
-                                || args.Outcome.Exception?.InnerException is TaskCanceledException
-                        );
-                    o.Retry.MaxRetryAttempts = 2;
-                    o.Retry.BackoffType = Polly.DelayBackoffType.Constant;
-                    o.Retry.UseJitter = false;
-                    o.Retry.Delay = TimeSpan.FromSeconds(40.0);
-                });
-        });
-        var kernel = evaluatorKernelBuilder.Build();
-
-        // add claim extraction plugin to the kernel
-        kernel.ImportPluginFromPromptDirectory(
-            Path.Combine(
-                AppDomain.CurrentDomain.BaseDirectory,
-                "..\\..\\..\\..\\Micronaire\\Claims\\ExtractorPlugins"
-            )
-        );
-
-        // add evaluation plugins to the kernel
-        kernel.ImportPluginFromPromptDirectory(
-            Path.Combine(
-                AppDomain.CurrentDomain.BaseDirectory,
-                "..\\..\\..\\..\\Micronaire\\LLMEvaluation\\Plugins"
-            )
-        );
-        return kernel;
     }
 }
